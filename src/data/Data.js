552786fab002b2217bc375fe7ff84dd1b1e3b06e
Shashwata Mondal <shashwata.mondal@protonmail.com>
2022-08-25 09:47:37 +0000

    Update 3 files

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import faker from "faker";
-
 //Pie
 export const dataPie = {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -29,41 +26,56 @@ export const dataPie = {
     ]
 };
 
+// Final dataset - hardcoded
 
-// Line
-export const dataLine = {
-    labels : ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "Dataset 1",
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: "rgb(255, 99, 132)",
-            backgroundColor: "rgba(255, 99, 132, 0.5)",
-            yAxisID: "y"
-        },
-        {
-            label: "Dataset 2",
-            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-            borderColor: "rgb(53, 162, 235)",
-            backgroundColor: "rgba(53, 162, 235, 0.5)",
-            yAxisID: "y1"
+export const options = {
+    responsive: true,
+    interaction: {
+        mode: "index",
+        intersect: false
+    },
+    stacked: false,
+    plugins: {
+            title: {
+                display: true,
+                text: "Gender"
+            }
+    },
+    scales: {
+        y: {
+            type: "linear",
+            display: true,
+            position: "left",
+            grid: {
+                drawOnChartArea: false
+            }
         }
-    ]
+    }
 };
 
-// Final dataset - hardcoded
-export const data = {
-    labels: [],
+export const dataLine = {
+    labels: ["Dec-20","Jan-21","Feb-21","Mar-21","Apr-21","May-21","June-21","July-21","Aug-21","Sept-21","Oct-21","Nov-21","Dec-21"],
     datasets: [
         {
-            label: "",
-            data: [],
-            backgroundColor: "",
-            borderColor: "",
-            yAxisID: "",
+            label: "Gender 1",
+            data: [0,0,0,0,0,47.29,0,47.02,0,0,0,0,0],
+            backgroundColor: "#fff",
+            borderColor: "rgba(255, 99, 132, 0.5)",
+            yAxisID: "y",
         },
         {
-            
+            label: "Gender 2",
+            data: [0,0,0,0,0,44.169,0,44.43,0,0,0,0,0],
+            backgroundColor: "#fff",
+            borderColor: "rgba(53, 162, 235, 0.5)",
+            yAxisID: "y",
+        },
+        {
+            label: "Gender 3",
+            data: [0,0,0,0,0,8.541,0,8.55,0,0,0,0,0],
+            backgroundColor: "#fff",
+            borderColor: "rgba(42, 42, 42, 0.5)",
+            yAxisID: "y",
         }
     ]
 };
--- src/scripts/Line.js
+++ src/scripts/Line.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -11,8 +10,7 @@ import {
   Legend
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-import faker from "faker"; //Used for data
-//import {dataLine} from "../data/Data";
+import { options, dataLine } from "../data/Data";
 
 ChartJS.register(
   CategoryScale,
@@ -24,59 +22,6 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  interaction: {
-    mode: "index",
-    intersect: false
-  },
-  stacked: false,
-  plugins: {
-    title: {
-      display: true,
-      text: "Chart.js Line Chart - Multi Axis"
-    }
-  },
-  scales: {
-    y: {
-      type: "linear",
-      display: true,
-      position: "left"
-    },
-    y1: {
-      type: "linear",
-      display: true,
-      position: "right",
-      grid: {
-        drawOnChartArea: false
-      }
-    }
-  }
-};
-
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-      yAxisID: "y"
-    },
-    {
-      label: "Dataset 2",
-      data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-      yAxisID: "y1"
-    }
-  ],
-};
-
-
 export function LineC() {
-  return <Line options={options} data={data} />;
+    return <Line options={options} data={dataLine} />;
 }
